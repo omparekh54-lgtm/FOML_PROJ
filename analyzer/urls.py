111e2f78c5073fb3a5a0359dae24bfd645a7b4a6
omparekh54-lgtm <omparekh54@gmail.com>
2026-09-25 13:47:21 +0530

    Sync analyzer/urls.py exactly from ZIP

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+
 from . import views
 
-app_name="analyzer"
-urlpatterns=[path("",views.index,name="index"),path("api/analyze/",views.api_analyze,name="api_analyze")]
+app_name = "analyzer"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("api/analyze/", views.api_analyze, name="api_analyze"),
+]
